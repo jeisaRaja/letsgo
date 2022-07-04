f2aeeda21ea126a68e4078ac3f9f42e9f9824b98
jeisa <jeisa.raja.jrt@gmail.com>
2022-07-04 15:41:14 +0700

    added flag control, bug exist

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	log.Println(*addr)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", snippet)
 	mux.HandleFunc("/snippet/create", create_snippet)
-	port := ":4000"
 	fileserver := http.FileServer(http.Dir("../../ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
-	err := http.ListenAndServe(port, mux)
-	log.Println("Starting server on port", port)
+	log.Println("Starting server on port", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
 	log.Println(err)
 }
