1f616b4c306a224d81d912dd4464abf316a09d64
jeisa <jeisa.raja.jrt@gmail.com>
2022-07-12 17:42:41 +0700

    added db pool and new models struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"jeisaRaja.git/snippetbox/pkg/models/mysql"
 )
 
 type application struct {
-	infoLog  *log.Logger
-	errorLog *log.Logger
+	infoLog    *log.Logger
+	errorLog   *log.Logger
+	snippetbox *mysql.SnippetModel
 }
 
 func main() {
@@ -20,8 +22,9 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errlog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	defaultDSN := "web:Cipinang01@/snippetbox?parseTime=true"
 	addr := flag.String("addr", "4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:Cipinang01@/snippetbox?parseTime=true", "MYSQL Database Pool")
+	dsn := flag.String("dsn", defaultDSN, "MYSQL Database Pool")
 
 	flag.Parse()
 
@@ -32,8 +35,9 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		infoLog:  infoLog,
-		errorLog: errlog,
+		infoLog:    infoLog,
+		errorLog:   errlog,
+		snippetbox: &mysql.SnippetModel{DB: db},
 	}
 	*addr = "127.0.0.1:" + *addr
 
@@ -45,6 +49,7 @@ func main() {
 
 	infoLog.Printf("Starting server on %s", *addr)
 	srv.ListenAndServe()
+
 }
 
 func openDB(dsn string) (*sql.DB, error) {
